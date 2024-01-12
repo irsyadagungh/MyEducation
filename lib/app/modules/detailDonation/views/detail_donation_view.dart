e59c4b70d56e7d4b6d61e0f1cb38f1f4234bc4ad
Maqilrp <aqilrasyid2004@gmail.com>
2024-01-12 13:33:49 +0700

    commit rute

--- lib/app/modules/detailDonation/views/detail_donation_view.dart
+++ lib/app/modules/detailDonation/views/detail_donation_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:my_education/app/data/models/content_donation_model.dart';
+import 'package:my_education/app/modules/payment/views/payment_view.dart';
 
 import '../controllers/detail_donation_controller.dart';
 
@@ -12,12 +12,36 @@ class DetailDonationView extends GetView<DetailDonationController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('DetailDonationView'),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: [Text("${item.title}")],
-        ));
+      appBar: AppBar(
+        title: const Text('DetailDonationView'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Text("${item.title}"),
+          Container(
+            width: double.infinity,
+            height: 50,
+            child: ElevatedButton(
+              onPressed: () {
+                Get.to(() => PaymentView());
+              },
+              child: Text("Donasi"),
+              style: ButtonStyle(
+                elevation: MaterialStateProperty.all(0),
+                backgroundColor: MaterialStateProperty.all(Colors.transparent),
+                foregroundColor: MaterialStateProperty.all(Colors.black),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    side: BorderSide(width: 1),
+                  ),
+                ),
+              ),
+            ),
+          ), // <-- Closing parenthesis for Container
+        ],
+      ),
+    );
   }
 }
